4d3dc55cbce811ff96f887e57f0a2aa57c637bfd
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-12-31 23:43:56 +0500

    init: next.js base setup with app router and base pages

---
+++ app/blog/next-routing-power/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>next-routing-basics</div>
+  )
+}
+
+export default page
---
+++ app/blog/why-nextjs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>why-nextjs</div>
+  )
+}
+
+export default page
