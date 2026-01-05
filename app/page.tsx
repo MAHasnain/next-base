112b70a65acd189bc49217aad9c2d06e0410380f
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-01-05 23:46:21 +0500

    feat: explore shadcn/ui components and integrate into Next.js pages
    - Set up shadcn/ui in Next.js project
    - Added and tested multiple UI components
    - Experimented with layout, buttons, cards, and forms
    - Improved overall UI structure for future development

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+"use client"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
-export default function Home() {
+export function page() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <h1>Home</h1>
-    </>
-  );
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm"
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
 }
+
+export default page
